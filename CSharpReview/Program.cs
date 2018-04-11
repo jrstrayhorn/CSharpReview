cf4b98203390b73719908fa8c13d4c9b241871e5
stepforwardtech <jrstrayhorn@gmail.com>
2018-04-11 07:44:26 -0400

    Finished DateTime Lecture

--- CSharpReview/Program.cs
+++ CSharpReview/Program.cs
@@ -10,7 +10,24 @@ namespace CSharpReview
     {
         static void Main(string[] args)
         {
-            
+            // DateTime is a struct
+            var dateTime = new DateTime(2015, 1, 1);
+            var now = DateTime.Now;
+            var today = DateTime.Today;
+
+            //Console.WriteLine($"Hour: {now.Hour}");
+            //Console.WriteLine($"Minute: {now.Minute}");
+
+            // DateTimes are immutable can't change unless with method
+            var tomorrow = now.AddDays(1);
+            var yesterday = now.AddDays(-1);
+
+            // Formatting to string
+            Console.WriteLine(now.ToLongDateString());
+            Console.WriteLine(now.ToShortDateString());
+            Console.WriteLine(now.ToLongTimeString());
+            Console.WriteLine(now.ToShortTimeString());
+            Console.WriteLine(now.ToString("yyyy-MM-dd HH:mm"));
         }
     }
 }
