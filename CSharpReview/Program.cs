e1d3ec4b01d62c92523940425283ad0649e39d5d
stepforwardtech <jrstrayhorn@gmail.com>
2018-04-11 07:51:18 -0400

    working on TimeSpan lecture

--- CSharpReview/Program.cs
+++ CSharpReview/Program.cs
@@ -10,6 +10,19 @@ namespace CSharpReview
     {
         static void Main(string[] args)
         {
+            // creating
+            var timeSpan = new TimeSpan(1, 2, 3);
+            var timeSpan1 = new TimeSpan(1, 0, 0);
+
+            // this one is more readable
+            var timeSpan2 = TimeSpan.FromHours(1);
+
+            var start = DateTime.Now;
+            var end = DateTime.Now.AddMinutes(2);
+            var duration = end - start;
+            Console.WriteLine($"duration: {duration}");
+
+            // properties
             
         }
     }
