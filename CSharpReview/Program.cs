b087fddecc88234cea337be3e40359b9f8f1f136
stepforwardtech <jrstrayhorn@gmail.com>
2018-04-12 17:27:15 -0400

    Finished with operators

--- CSharpReview/Program.cs
+++ CSharpReview/Program.cs
@@ -10,7 +10,30 @@ namespace CSharpReview
     {
         static void Main(string[] args)
         {
-            
+            // arimetic operators
+            var a = 10;
+            var b = 3;
+
+            Console.WriteLine(a + b);
+
+            Console.WriteLine((float)a + (float)b);
+
+            var c = 4;
+
+            Console.WriteLine((a + b) * c);
+
+            // comparision
+            Console.WriteLine(a > b);
+            Console.WriteLine(a == b);
+            Console.WriteLine(a != b);
+            Console.WriteLine(!(a != b)); // sames as a ==b
+
+            // logical
+            Console.WriteLine(c > b && c > a);
+            Console.WriteLine(c > b || c == a);
+            Console.WriteLine(!(c > b || c == a));
+
+
         }
     }
 }
