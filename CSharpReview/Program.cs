12e3bc094ed5584614fd7ff963e3d716caa782cc
stepforwardtech <jrstrayhorn@gmail.com>
2018-04-12 17:57:00 -0400

    Finished with strings

--- CSharpReview/Program.cs
+++ CSharpReview/Program.cs
@@ -10,7 +10,31 @@ namespace CSharpReview
     {
         static void Main(string[] args)
         {
-            
+            var firstName = "JR";
+            // each primitive type maps to struct in .NET framework
+            // like int is System.Int32
+
+            // string is a CLASS, both of these ways work
+            string lastName = "Strayhorn";
+            String anotherName = "Strayhorn"; // must add the System namespace to use this
+
+            var myLastName = "Stray";
+
+            //var fullName = firstName + " " + lastName; or
+            //var fullName = string.Format("My name is {0} {1}", firstName, lastName); or
+            var fullName = $"{firstName} {lastName}";
+
+            // join
+            var names = new string[3] { "John", "Jack", "Mary" };
+            var formattedNames = string.Join(",", names);
+            Console.WriteLine(formattedNames);
+
+            // verbatim  \n - Is new line char
+            var text = @"Hi John
+Look into the following paths
+c:\folder1\folder2\folder3";
+
+            Console.WriteLine(text);
         }
     }
 }
