df46247865c06d61caa6e55c47b8ea41b1366ff7
stepforwardtech <jrstrayhorn@gmail.com>
2018-04-13 17:57:34 -0400

    Finished with While Loops

--- CSharpReview/Program.cs
+++ CSharpReview/Program.cs
@@ -10,7 +10,60 @@ namespace CSharpReview
     {
         static void Main(string[] args)
         {
-            
+            for (int i = 1; i <= 10; i++)
+            {
+                if (i%2 == 0)
+                {
+                    Console.WriteLine(i);
+                }
+            }
+
+            // can use while loops for for-loops as well
+            // for loops is good when you know how many times
+            // you want to iterate
+            // typically use while loop when you don't know
+            // ahead of time how many iterations
+            var i = 0;
+            while (i <= 10)
+            {
+                if (i%2 == 0)
+                {
+                    Console.WriteLine(i);
+                }
+
+                i++;
+            }
+
+            // means will run forever
+            while (true)
+            {
+                Console.Write("Type your name: ");
+                var input = Console.ReadLine();
+
+                if (String.IsNullOrWhiteSpace(input))
+                {
+                    break;
+                }
+
+                Console.WriteLine("@Echo: " + input);
+            }
+
+            // means will run forever
+            while (true)
+            {
+                Console.Write("Type your name: ");
+                var input = Console.ReadLine();
+
+                if (!String.IsNullOrWhiteSpace(input))
+                {
+                    Console.WriteLine("@Echo: " + input);
+                    continue; // will go back to the beginning of the loop
+                }
+
+                break;
+            }
+
+
         }
     }
 }
