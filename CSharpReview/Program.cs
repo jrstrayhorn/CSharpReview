4f97802e7c63f4682dd43220d35fdfa92f01f829
stepforwardtech <jrstrayhorn@gmail.com>
2018-04-13 17:29:16 -0400

    Finished with For Loops

--- CSharpReview/Program.cs
+++ CSharpReview/Program.cs
@@ -10,7 +10,21 @@ namespace CSharpReview
     {
         static void Main(string[] args)
         {
-            
+            for (int i = 1; i <= 10; i++)
+            {
+                if (i%2 == 0)
+                {
+                    Console.WriteLine(i);
+                }
+            }
+
+            for (int j = 10; j >= 1; j--)
+            {
+                if (j%2 == 0)
+                {
+                    Console.WriteLine(j);
+                }
+            }
         }
     }
 }
