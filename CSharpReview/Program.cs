792c19ecb0b662fea9dcbc6da53ea69e6fa35d19
stepforwardtech <jrstrayhorn@gmail.com>
2018-04-13 17:34:36 -0400

    Finished with foreach loop

--- CSharpReview/Program.cs
+++ CSharpReview/Program.cs
@@ -10,7 +10,27 @@ namespace CSharpReview
     {
         static void Main(string[] args)
         {
-            
+            var name = "John Smith";
+            // string is enumerable
+            // just a list of chars
+
+            for (int i = 0; i < name.Length; i++)
+            {
+                Console.WriteLine(name[i]);
+            }
+
+            // foreach is much cleaner and easier to read
+            // string, array, list use a foreach
+            foreach (var character in name)
+            {
+                Console.WriteLine(character);
+            }
+
+            var numbers = new int[] { 1, 2, 3, 4 };
+            foreach (var number in numbers)
+            {
+                Console.WriteLine(number);
+            }
         }
     }
 }
