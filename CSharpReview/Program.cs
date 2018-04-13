4e5dffcba7ad6fe4447621077a7dc870b2518de1
stepforwardtech <jrstrayhorn@gmail.com>
2018-04-13 07:29:41 -0400

    Finished with Ref and Values Types 2

--- CSharpReview/Program.cs
+++ CSharpReview/Program.cs
@@ -6,11 +6,36 @@ using System.Threading.Tasks;
 
 namespace CSharpReview
 {
+    public class Person
+    {
+        public int Age;
+    }
+
     class Program
     {
         static void Main(string[] args)
         {
-            
+            var number = 1;
+            Increment(number);
+            // number will be 1 because passed by value
+            // number in increment is not passed back
+            Console.WriteLine(number);
+
+            var person = new Person() { Age = 20 };
+            // object is passed by reference
+            // so MakeOld WILL update the Age property
+            MakeOld(person);
+            Console.WriteLine(person.Age);
+        }
+
+        public static void Increment(int number)
+        {
+            number += 10;
+        }
+
+        public static void MakeOld(Person person)
+        {
+            person.Age += 10;
         }
     }
 }
