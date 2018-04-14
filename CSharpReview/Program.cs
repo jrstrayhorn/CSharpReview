1d9dfb950eb2e0e19bc4bac49a642894255a27a4
stepforwardtech <jrstrayhorn@gmail.com>
2018-04-14 17:06:31 -0400

    Finiished with Strings

--- CSharpReview/Program.cs
+++ CSharpReview/Program.cs
@@ -10,6 +10,37 @@ namespace CSharpReview
     {
         static void Main(string[] args)
         {
+            var fullName = "JR Strayhorn ";
+
+            // should trim user input
+            Console.WriteLine("Trim: '{0}'", fullName.Trim());
+
+            Console.WriteLine("ToUpper: '{0}'", fullName.Trim().ToUpper());
+
+            var index = fullName.IndexOf(' ');
+            var firstName = fullName.Substring(0, index);
+            var lastName = fullName.Substring(index + 1);
+            Console.WriteLine("FirstName: " + firstName);
+            Console.WriteLine("LastName: " + lastName);
+
+            var names = fullName.Split(' ');
+            Console.WriteLine("FirstName: " + names[0]);
+            Console.WriteLine("LastName: " + names[1]);
+
+            Console.WriteLine(fullName.Replace("JR", "James"));
+
+            if (String.IsNullOrWhiteSpace(" "))
+            {
+                Console.WriteLine("Invalid");
+            }
+
+            // will need to convert user string to int
+            var str = "25";
+            var age = Convert.ToByte(str);
+            Console.WriteLine(age);
+
+            float price = 29.95f;
+            Console.WriteLine(price.ToString("C0"));    // toString is available every where
             
         }
     }
