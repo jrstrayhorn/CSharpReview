a295e3b4b255ea05132116fb76fc7904cd8c9476
stepforwardtech <jrstrayhorn@gmail.com>
2018-04-14 09:35:53 -0400

    Finished with Random Class

--- CSharpReview/Program.cs
+++ CSharpReview/Program.cs
@@ -10,6 +10,29 @@ namespace CSharpReview
     {
         static void Main(string[] args)
         {
+            var random = new Random();
+            for (int i = 0; i < 10; i++)
+            {
+                Console.WriteLine(random.Next(1, 10));
+            }
+
+            // random number between 1 and 10
+            //random.Next(1, 10);
+
+            Console.WriteLine((int)'a');  // 97 which represent lower case a on computer
+                                          // ascii-code.com leter to number conversion
+
+            // generate random password
+            const int passwordLength = 10;
+            var buffer = new char[passwordLength];
+            for (int i = 0; i < passwordLength; i++)
+            {
+                buffer[i] = ((char)('a' + random.Next(0, 26)));  
+                // will result in number between a and z
+            }
+
+            var password = new string(buffer);
+            Console.WriteLine(password);
             
         }
     }
