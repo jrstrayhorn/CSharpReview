19436d707e474cb6fb76e514ade44bd220e95831
stepforwardtech <jrstrayhorn@gmail.com>
2018-04-14 16:32:33 -0400

    Finished with Arrays

--- CSharpReview/Program.cs
+++ CSharpReview/Program.cs
@@ -10,7 +10,52 @@ namespace CSharpReview
     {
         static void Main(string[] args)
         {
-            
+            var numbers = new [] { 3, 7, 9, 2, 14, 6 };
+
+            // length
+            Console.WriteLine("Length: " + numbers.Length);
+
+            // IndexOf() - find index of element of array
+            var index = Array.IndexOf(numbers, 9);
+            Console.WriteLine(index);
+
+            // Clear - sets items to default
+            Array.Clear(numbers, 0, 2);
+
+            foreach (var n in numbers)
+            {
+                Console.WriteLine(n);
+            }
+
+            // Copy
+            int[] another = new int[3];
+            Array.Copy(numbers, another, 3);
+
+            Console.WriteLine("Effect of Copy()");
+            foreach (var n in another)
+            {
+                Console.WriteLine(n);
+            }
+
+            // Sort()
+            Array.Sort(another);
+
+            Console.WriteLine("Effect of Sort()");
+            foreach (var n in another)
+            {
+                Console.WriteLine(n);
+            }
+
+            // Reverse()
+            Array.Reverse(numbers);
+
+            Console.WriteLine("Effect of Reverse()");
+            foreach (var n in numbers)
+            {
+                Console.WriteLine(n);
+            }
+
+
         }
     }
 }
