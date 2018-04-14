765a2970bacaf613fe1a4fe90840c78842a41d17
stepforwardtech <jrstrayhorn@gmail.com>
2018-04-14 17:20:40 -0400

    Finished with Summaring Text example

--- CSharpReview/Program.cs
+++ CSharpReview/Program.cs
@@ -1,8 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace CSharpReview
 {
@@ -10,7 +6,9 @@ namespace CSharpReview
     {
         static void Main(string[] args)
         {
-            
+            var sentence = "This is going to be a really really really really really really long text.";
+            var summary = StringUtility.SummarizeText(sentence, 25);
+            Console.WriteLine(summary);
         }
     }
 }
---
+++ CSharpReview/StringUtility.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace CSharpReview
+{
+    public class StringUtility
+    {
+        public static string SummarizeText(string sentence, int maxLength = 20)
+        {
+            if (sentence.Length < maxLength)
+            {
+                return sentence;
+            }
+
+            var words = sentence.Split(' ');
+            var totalCharacters = 0;
+            var summaryWords = new List<string>();
+
+            foreach (var word in words)
+            {
+                summaryWords.Add(word);
+
+                totalCharacters += word.Length + 1;
+                if (totalCharacters > maxLength)
+                {
+                    break;
+                }
+            }
+
+            return String.Join(" ", summaryWords) + "...";
+        }
+    }
+}
