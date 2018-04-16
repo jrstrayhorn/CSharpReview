274d114081540822c7f3af8e0427a1bbd98d5cd4
stepforwardtech <jrstrayhorn@gmail.com>
2018-04-16 17:23:59 -0400

    Starting code

--- CSharpReview/Program.cs
+++ CSharpReview/Program.cs
@@ -10,7 +10,17 @@ namespace CSharpReview
     {
         static void Main(string[] args)
         {
-            
+            Console.WriteLine("What's your name?");
+            var name = Console.ReadLine();
+
+            var array = new char[name.Length];
+            for (var i = name.Length; i > 0; i--)
+            {
+                array[name.Length - i] = name[i - 1];
+            }
+
+            var reversed = new string(array);
+            Console.WriteLine("Reversed name: " + reversed);
         }
     }
 }
