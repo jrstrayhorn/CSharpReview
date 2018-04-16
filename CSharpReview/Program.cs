525a3e8da351f85da2b54dcb3291c006d6c237e7
stepforwardtech <jrstrayhorn@gmail.com>
2018-04-16 17:31:27 -0400

    Finished with 1st refactoring

--- CSharpReview/Program.cs
+++ CSharpReview/Program.cs
@@ -8,19 +8,38 @@ namespace CSharpReview
 {
     class Program
     {
+        // rule of thumb separater code that works
+        // with console from code that works with
+        // logic
+        // real world hae different presentation
+        // extracting logic will allow reusability
+        // console only relevant in console
+        
+        // responsible for getting input and display output
         static void Main(string[] args)
         {
+            // get user name
             Console.WriteLine("What's your name?");
             var name = Console.ReadLine();
 
+            // reverse logic
+            var reversed = ReverseName(name);
+
+            // show on console
+            Console.WriteLine("Reversed name: " + reversed);
+        }
+        
+        // use meaningful method name
+        // responsible for reversing
+        public static string ReverseName(string name)
+        {
             var array = new char[name.Length];
             for (var i = name.Length; i > 0; i--)
             {
                 array[name.Length - i] = name[i - 1];
             }
 
-            var reversed = new string(array);
-            Console.WriteLine("Reversed name: " + reversed);
+            return new string(array);
         }
     }
 }
