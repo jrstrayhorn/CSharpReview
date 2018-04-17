3e49be930c023cb1c119767b3e79e0da31669ad8
stepforwardtech <jrstrayhorn@gmail.com>
2018-04-17 17:47:43 -0400

    Finished with Directory / Directory Info

--- CSharpReview/Program.cs
+++ CSharpReview/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,7 +11,30 @@ namespace CSharpReview
     {
         static void Main(string[] args)
         {
-            
+            // Directory provide static methods
+            // Directory provide instance methods
+
+            Directory.CreateDirectory(@"c:/somepath");
+
+            var files = Directory.GetFiles(@"c:\projects\coding", "*.exe", SearchOption.AllDirectories);
+
+            foreach (var file in files)
+            {
+                Console.WriteLine(file);
+            }
+
+            var directories = Directory.GetDirectories(@"c:\projects\coding", "*.*", SearchOption.AllDirectories);
+
+            foreach (var dir in directories)
+            {
+                Console.WriteLine(dir);
+            }
+
+            Directory.Exists("...");
+
+            var directoryInfo = new DirectoryInfo("...");
+            directoryInfo.GetFiles();
+            directoryInfo.GetDirectories();
         }
     }
 }
