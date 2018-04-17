f903adc5680e25f2a397cf9f8bef545bc80218d2
stepforwardtech <jrstrayhorn@gmail.com>
2018-04-17 18:00:28 -0400

    Finished with Path

--- CSharpReview/Program.cs
+++ CSharpReview/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,7 +11,12 @@ namespace CSharpReview
     {
         static void Main(string[] args)
         {
-            
+            var path = @"C:\Projects\Coding\HelloWorld\HelloWorld.sln";
+
+            Console.WriteLine("Extension: " + Path.GetExtension(path));
+            Console.WriteLine("File Name: " + Path.GetFileName(path));
+            Console.WriteLine("File Name without Extension: " + Path.GetFileNameWithoutExtension(path));
+            Console.WriteLine("Directory Name: " + Path.GetDirectoryName(path));
         }
     }
 }
