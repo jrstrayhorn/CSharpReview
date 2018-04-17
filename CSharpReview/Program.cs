6b93445d0c8fcb7513be74fdb5dacd15382ec395
stepforwardtech <jrstrayhorn@gmail.com>
2018-04-17 17:41:28 -0400

    Finished with File and File Info

--- CSharpReview/Program.cs
+++ CSharpReview/Program.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.IO;
 
 namespace CSharpReview
 {
@@ -10,7 +6,35 @@ namespace CSharpReview
     {
         static void Main(string[] args)
         {
+            // File provide static methods - small methods against one file
+            // FileInfo provide instance methods - if doing many operations against files
+
+            var path = @"C:\somefile.jpg";
+
+            File.Copy(@"c:\temp\myfile.jpg", @"d:\temp\myfile.jpg", true);
+
+            File.Delete(path);
+
+            if (File.Exists(path))
+            {
+                //
+            }
+
+            var content = File.ReadAllText(path);
+
+            var fileInfo = new FileInfo(path);
+
+            fileInfo.CopyTo("...", true);
+
+            fileInfo.Delete();
+
+            if (fileInfo.Exists)
+            {
+                //
+            }
             
+            // fileInfo has no readall method like File
+
         }
     }
 }
