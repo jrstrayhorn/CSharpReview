e75e26e86c696b106ed57ece877c22bedd8826df
stepforwardtech <jrstrayhorn@gmail.com>
2018-04-18 17:30:41 -0400

    WIP

--- CSharpReview/Program.cs
+++ CSharpReview/Program.cs
@@ -8,9 +8,47 @@ namespace CSharpReview
 {
     class Program
     {
-        static void Main(string[] args)
+        /*
+         F9 - Place/Remove Breakpoint
+         F5 - Start Debugging
+         Shift + F5 - Stop Debug Session
+         F10 - Step over (goes to next line in execution)
+
+    */
+        public static void Main(string[] args)
         {
-            
+            var numbers = new List<int> { 1, 2, 3, 4, 5, 6 };
+            var smallests = GetSmallests(numbers, 3);
+
+            foreach (var number in smallests)
+                Console.WriteLine(number);
         }
+
+        public static List<int> GetSmallests(List<int> list, int count)
+        {
+            var smallests = new List<int>();
+
+            while (smallests.Count < count)
+            {
+                var min = GetSmallest(list);
+                smallests.Add(min);
+                list.Remove(min);
+            }
+
+            return smallests;
+        }
+
+        public static int GetSmallest(List<int> list)
+        {
+            // Assume the first number is the smallest
+            var min = list[0];
+            for (var i = 1; i < list.Count; i++)
+            {
+                if (list[i] > min)
+                    min = list[i];
+            }
+            return min;
+        }
+
     }
 }
