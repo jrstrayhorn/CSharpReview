46ee5b5358202c53db2362ce20cfcbb10cdc93a4
stepforwardtech <jrstrayhorn@gmail.com>
2018-04-20 17:54:33 -0400

    Finished with Intro

--- CSharpReview/Program.cs
+++ CSharpReview/Program.cs
@@ -6,11 +6,29 @@ using System.Threading.Tasks;
 
 namespace CSharpReview
 {
+    public class Person
+    {
+        public string Name;
+
+        public void Introduce(string to)
+        {
+            Console.WriteLine($"Hi {to}, I am {Name}");
+        }
+
+        public static Person Parse(string str)
+        {
+            return new Person() { Name = str };
+        }
+    }
+
     class Program
     {
         static void Main(string[] args)
         {
-            
+            //var person = new Person(); // compile will figure out the type
+            var person = Person.Parse("John");
+            //person.Name = "John";
+            person.Introduce("Mike");
         }
     }
 }
