91f94bc667dbed7379fa6ebd8f907839fe0f048d
stepforwardtech <jrstrayhorn@gmail.com>
2018-04-21 18:27:40 -0400

    Finished with Access Modifiers

---
+++ CSharpReview/Person.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+// Encapuslation should hide internal details
+// should only show what they have, not how they get to it
+namespace CSharpReview
+{
+    public class Person
+    {
+        private DateTime _birthdate;  // can't access birthdate outside the Person class
+
+        public void SetBirthdate(DateTime birthdate)
+        {
+            // can implement logic about Birthdate here
+            _birthdate = birthdate;
+        }
+
+        public DateTime GetBirthdate()
+        {
+            return _birthdate;
+        }
+    }
+}
--- CSharpReview/Program.cs
+++ CSharpReview/Program.cs
@@ -10,7 +10,9 @@ namespace CSharpReview
     {
         static void Main(string[] args)
         {
-            
+            var person = new Person();
+            person.SetBirthdate(new DateTime(1982, 1, 1));
+            Console.WriteLine(person.GetBirthdate());
         }
     }
 }
