af23c5389ff68f82e568aa32070a96e8a24e5d1c
stepforwardtech <jrstrayhorn@gmail.com>
2018-04-21 18:15:26 -0400

    Finished with Fields

---
+++ CSharpReview/Customer.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpReview
+{
+    public class Customer
+    {
+        public int Id;
+        public string Name;
+
+        // if you have a list should always
+        // initialize to an empty list
+        public readonly List<Order> Orders = new List<Order>(); // this will only be set once!!!
+
+        public Customer(int id)
+        {
+            this.Id = id;
+        }
+
+        public Customer(int id, string name)
+            : this(id)
+        {
+            this.Name = name;
+        }
+
+        public void Promote()
+        {
+            // .....
+        }
+
+    }
+}
--- CSharpReview/Program.cs
+++ CSharpReview/Program.cs
@@ -10,7 +10,13 @@ namespace CSharpReview
     {
         static void Main(string[] args)
         {
-            
+            var customer = new Customer(1);
+            customer.Orders.Add(new Order());
+            customer.Orders.Add(new Order());
+
+            customer.Promote();
+
+            Console.WriteLine(customer.Orders.Count);
         }
     }
 }
