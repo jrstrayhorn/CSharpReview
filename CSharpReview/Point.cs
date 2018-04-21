d069757b90cbeaf3a2f746f9c4582007c1861c1b
stepforwardtech <jrstrayhorn@gmail.com>
2018-04-21 17:48:12 -0400

    Finished with Methods

---
+++ CSharpReview/Point.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpReview
+{
+    public class Point
+    {
+        public int X;
+        public int Y;
+
+        public Point(int x, int y)
+        {
+            this.X = x;
+            this.Y = y;
+        }
+
+        public void Move(int x, int y)
+        {
+            this.X = x;
+            this.Y = y;
+        }
+
+        // overload
+        public void Move(Point newLocation)
+        {
+            // defensive programming
+            // improves robustness of code
+            if (newLocation == null)
+            {
+                throw new ArgumentNullException(nameof(newLocation));
+            }
+
+            Move(newLocation.X, newLocation.Y);
+        }
+    }
+}
--- CSharpReview/Program.cs
+++ CSharpReview/Program.cs
@@ -6,11 +6,55 @@ using System.Threading.Tasks;
 
 namespace CSharpReview
 {
+    
     class Program
     {
         static void Main(string[] args)
         {
-            
+            try
+            {
+                var num = int.Parse("abc"); //will crash
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("Conversion failed");
+            }
+
+            var result = int.TryParse("abc", out int number);
+            if (result)
+            {
+                Console.WriteLine(number);
+            }
+            else
+            {
+                Console.WriteLine("Conversion failed");
+            }
+        }
+
+        private static void UseParams()
+        {
+            var calculator = new Calculator();
+            Console.WriteLine(calculator.Add(1, 2));
+            Console.WriteLine(calculator.Add(1, 2, 3));
+            Console.WriteLine(calculator.Add(new int[] { 1, 2, 3, 4, 5, 6 }));
+        }
+
+        static void UsePoints()
+        {
+            try
+            {
+                var point = new Point(10, 20);
+                //point.Move(new Point(40, 60));
+                point.Move(null);
+                Console.WriteLine($"Point is at ({point.X}, {point.Y})");
+
+                point.Move(100, 200);
+                Console.WriteLine($"Point is at ({point.X}, {point.Y})");
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("An unexpected error occured.");
+            }
         }
     }
 }
