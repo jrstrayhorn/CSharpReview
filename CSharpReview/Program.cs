a0f9267adb860a805cd94069b14daaae86d2a04f
stepforwardtech <jrstrayhorn@gmail.com>
2018-05-01 07:43:07 -0400

    WIP - Exercises

--- CSharpReview/Program.cs
+++ CSharpReview/Program.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace CSharpReview
@@ -10,7 +11,35 @@ namespace CSharpReview
     {
         static void Main(string[] args)
         {
-            
+            TryStopwatch();
+        }
+
+        private static void TryStopwatch()
+        {
+            var stopwatch = new Stopwatch();
+            Console.WriteLine(stopwatch.Duration);
+
+            stopwatch.Start();
+
+            Console.WriteLine("Pausing for 10 seconds");
+            Thread.Sleep(10000);
+
+            stopwatch.Stop();
+
+            Console.WriteLine($"Duration: {stopwatch.Duration}");
+
+            stopwatch.Start();
+
+            Console.WriteLine("Pausing for 5 seconds");
+            Thread.Sleep(5000);
+
+            stopwatch.Stop();
+            stopwatch.Stop();
+
+            Console.WriteLine($"Duration: {stopwatch.Duration}");
+
+            //stopwatch.Start();
+            //stopwatch.Start();
         }
     }
 }
---
+++ CSharpReview/Stopwatch.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpReview
+{
+    public class Stopwatch
+    {
+        private bool _isStarted;
+        private DateTime _start;
+        private DateTime _end;
+
+        public Stopwatch()
+        {
+            _isStarted = false;
+            _start = DateTime.MinValue;
+            _end = DateTime.MinValue;
+        }
+
+        public string Duration
+        {
+            get { return _end.Subtract(_start).ToString(@"h\:mm\:ss"); }
+        }
+
+        public void Start()
+        {
+            if (_isStarted)
+            {
+                throw new InvalidOperationException("Stopwatch is already started.");
+            }
+
+            _isStarted = true;
+            _start = DateTime.Now;
+        }
+
+        public void Stop()
+        {
+            if (_isStarted)
+            {
+                _isStarted = false;
+                _end = DateTime.Now; 
+            }
+        }
+    }
+}
