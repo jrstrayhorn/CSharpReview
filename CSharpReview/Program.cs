c49b995b58fa863219a2ec8fce9f05b8f59c130f
stepforwardtech <jrstrayhorn@gmail.com>
2018-05-04 06:41:37 -0400

    Finished

--- CSharpReview/Program.cs
+++ CSharpReview/Program.cs
@@ -6,11 +6,17 @@ using System.Threading.Tasks;
 
 namespace CSharpReview
 {
+    
+
     class Program
     {
         static void Main(string[] args)
         {
-            
+            var text = new Text
+            {
+                Width = 100
+            };
+            text.Copy();
         }
     }
 }
