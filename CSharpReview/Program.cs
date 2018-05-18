a8800977e6e720d297ac5de3899725c7c8f4566a
stepforwardtech <jrstrayhorn@gmail.com>
2018-05-18 18:28:24 -0400

    done

--- CSharpReview/Program.cs
+++ CSharpReview/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,7 +11,23 @@ namespace CSharpReview
     {
         static void Main(string[] args)
         {
-            
+            var list = new ArrayList();
+            list.Add(1);
+            list.Add("JR");
+
+            // this is bad
+            // no type safety, will get errors at run time
+            // another problem add method takes parameter of object
+            // boxing will happen and cause performance hits
+
+            var anotherList = new List<int>();
+            // now this is type safe
+            anotherList.Add(3);
+            // no boxing b/c we store list of int
+
+
+
+
         }
     }
 }
