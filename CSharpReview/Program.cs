d5fd4277e564a06ab34a08e133df1a5a1f690836
stepforwardtech <jrstrayhorn@gmail.com>
2018-06-12 06:51:10 -0400

    wip

--- CSharpReview/Program.cs
+++ CSharpReview/Program.cs
@@ -10,7 +10,40 @@ namespace CSharpReview
     {
         static void Main(string[] args)
         {
-            
+            // args => expression  args goes to expreession
+            // number => number*number same as below method
+            // compilter automatically can infer types
+
+            // Func is a pointer to method that takes in types and return type
+            // Action is a pointer to method that takes in types and return void
+
+            // we are defining a delegate that will take an int as parameter and 
+            // return an int
+            //Func<int, int> square = Square;
+            //Func<int, int> square = number => number * number;
+
+            // No args () =>...
+            // 1 arg x => ...
+            // multiple args (x, y, z) => ...
+
+            //Console.WriteLine(square(5));
+
+            //const int factor = 5;
+
+            //Func<int, int> multipler = n => n * factor;
+
+            //var result = multipler(10);
+
+            //Console.WriteLine(result);
+
+            var books = new BookRepository().GetBooks();
+
+
         }
+
+        //static int square(int number)
+        //{
+        //    return number * number;
+        //}
     }
 }
