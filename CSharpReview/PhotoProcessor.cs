de4b3e81642d6f9b8eb46d3db455fa0d2451be63
stepforwardtech <jrstrayhorn@gmail.com>
2018-06-12 06:38:40 -0400

    done

---
+++ CSharpReview/PhotoProcessor.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpReview
+{
+    public class PhotoProcessor
+    {
+        // any functions that you point to via this delegate
+        // must follow the method signature defined
+        // in this case return type void with parameter of Photo
+        // so, void DoSomething(Photo photo) this can be referenced
+        // via this delegate
+        //public delegate void PhotoFilterHandler(Photo photo);
+
+        //public void Process(string path, PhotoFilterHandler filterHandler)
+        public void Process(string path, Action<Photo> filterHandler)
+        {
+            // this built-in delegate points to method(s) that return void
+            //System.Action<>
+
+            // this built-in delegate points to method(s) that return a value
+            //System.Func<in T, out TResult>  TResult specify the return value
+
+            // with these built in delegates theres really no reason to create
+            // your own custom delegates
+
+            var photo = Photo.Load(path);
+
+            // client will define the filters that they want to use
+            // now its extensible
+            filterHandler(photo);
+
+            // this code is NOT extensible
+            // what if someone wants to define a
+            // new filter that doesn't exist
+            // for a new filter, we have to recompile the code
+            // with delegate you can design
+            // with extensibility in mind  (can also use an interface)
+            //var filters = new PhotoFilters();
+            //filters.ApplyBrightness(photo);
+            //filters.ApplyContrast(photo);
+            //filters.Resize(photo);
+
+            photo.Save();
+        }
+    }
+}
--- CSharpReview/Program.cs
+++ CSharpReview/Program.cs
@@ -10,7 +10,23 @@ namespace CSharpReview
     {
         static void Main(string[] args)
         {
-            
+            var processor = new PhotoProcessor();
+            var filters = new PhotoFilters();
+
+            //PhotoProcessor.PhotoFilterHandler filterHandler = filters.ApplyBrightness;
+            Action<Photo> filterHandler = filters.ApplyBrightness;
+            filterHandler += filters.ApplyContrast; // += here add another pointer to function
+            filterHandler += RemoveRedEyeFilter;
+
+            // now you can create your own filter
+
+            processor.Process("photo.jpg", filterHandler);
+        }
+
+        // custom filter
+        static void RemoveRedEyeFilter(Photo photo)
+        {
+            Console.WriteLine("Apply RemoveRedEye");
         }
     }
 }
