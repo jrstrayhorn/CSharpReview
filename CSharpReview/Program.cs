c8fd7288cd82456c33e130d119ed2ebaec9490d2
stepforwardtech <jrstrayhorn@gmail.com>
2018-06-19 07:23:37 -0400

    done

--- CSharpReview/Program.cs
+++ CSharpReview/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,7 +11,56 @@ namespace CSharpReview
     {
         static void Main(string[] args)
         {
-            
+            StreamReader streamReader = null;
+            try
+            {
+                streamReader = new StreamReader(@"c:\file.zip");
+                var calculator = new Calculator();
+                var result = calculator.Divide(5, 0);
+
+                var content = streamReader.ReadToEnd();
+            }
+            //catch(DivideByZeroException ex) // most specific
+            //{
+            //    // can do something specific to this exception
+            //    Console.WriteLine("You cannot divide by 0.");
+            //}
+            //catch (ArithmeticException ex)
+            //{
+
+            //}
+            catch (Exception ex) // most generic, if this was first would hit any other catch block.
+            {
+                Console.WriteLine("Sorry, an unexpected error occurred.");
+            }
+            finally
+            {
+                // manual clean up
+                // files, database, network connection, graphics
+                // make sure to dispose with .Dispose()
+                if (streamReader != null)
+                {
+                    streamReader.Dispose();
+                }
+            }
+
+            // a cleaner way to write the above
+            try
+            {
+                //using (var myReader = new StreamReader(@"c:\file.zip"))
+                //{
+                //    var content = myReader.ReadToEnd();
+                //}
+                var api = new YouTubeApi();
+                var videos = api.GetVideos("jr");
+            }
+            catch (Exception ex)
+            {
+                //Console.WriteLine("Sorry, an unexpected error occurred.");
+                Console.WriteLine(ex.Message);
+            }
+
+
         }
     }
 }
---
+++ CSharpReview/YouTubeApi.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpReview
+{
+    public class YouTubeApi
+    {
+        public List<Video> GetVideos(string user)
+        {
+            try
+            {
+                // access youtube web service
+                // read the data
+                // create a list of video objects
+                throw new Exception("OOps some low level YouTube error.");
+            }
+            catch (Exception ex)
+            {
+                // Log
+
+                throw new YouTubeException("Could not fetch the videos from YouTube.", ex);
+            }
+
+            return new List<Video>();
+        }
+    }
+}
